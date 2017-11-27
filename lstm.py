42c24702aa5074fbb6f166fa4965b641d29073f9
Eric Martin <eric@ericmart.in>
2017-11-26 22:49:53 -0600

    Confirmed working

--- lstm.py
+++ lstm.py
@@ -7,13 +7,19 @@ import sympy
 def sigmoid(x):
     return 1. / (1. + np.exp(-x))
 
+def isigmoid(x):
+    return -np.log((1. / x) - 1.)
+
+def itanh(x):
+    return 0.5 * np.log((1. + x) / (1. - x))
+
 class Lstm(object):
     def __init__(self, hidden_size):
         self.N = N = hidden_size
         assert N % 2 == 0
         self.W = np.random.randn(4 * N, 2 * N)
 
-        self.h = np.zeros(N)
+        self.h = np.random.randn(N)
 
         # 3X f0, 3X f1. 1X i0z0, 1X i1z1, 2X i0z1, 2X i1z0
         self.c = OrderedDict(
@@ -30,15 +36,24 @@ class Lstm(object):
         self._lr_solver = build_solver(self.c.keys())
         self._iz_solver = self._gen_iz_solver()
 
-    def combine_c(self, upper=True):
+    @classmethod
+    def combine_c_half(cls, c_dict, upper=True):
         i = 0 if upper else 1
-        c = [v for (idx, v) in self.c.items() if idx[0] == i]
+        c = [v for (idx, v) in c_dict.items() if idx[0] == i]
         assert len(c) == 3
         return np.mean(np.stack(c, axis=1), axis=1)
+
+    @classmethod
+    def combine_c(cls, c_dict):
+        return np.concatenate([
+            cls.combine_c_half(c_dict, upper=True),
+            cls.combine_c_half(c_dict, upper=False)
+        ])
 
     def forward(self, x):
         assert x.size == self.N
         inp = np.concatenate([self.h, x], axis=0)
+        print inp
         preact = np.dot(self.W, inp)
 
         f, i, z, o = np.split(preact, 4)
@@ -54,18 +69,17 @@ class Lstm(object):
         for idx in self.c.keys():
             this_z = z[idx[2]]
             if idx[1] != idx[2]:
-                pass
-                #this_z *= (np.abs(this_z) ** .1)
+                # hack to produce non-singular system for iz
+                this_z = this_z * (np.abs(this_z) ** .1)
+
             self.c[idx] = f[idx[0]] * self.c[idx] + i[idx[1]] * this_z
 
-        c = np.concatenate([
-            self.combine_c(upper=True),
-            self.combine_c(upper=False)
-        ])
+        c = self.combine_c(self.c)
         self.h = o * c
 
-    @staticmethod
-    def _gen_iz_solver():
+    @classmethod
+    def _gen_iz_solver(cls):
+        # contains 1.1 values to be non-singular
         A = sympy.Matrix(
             [[1, 0, 1, 0],
              [1, 0, 0, 1.1],
@@ -83,19 +97,39 @@ class Lstm(object):
     def backward(self, c_tm1, c_t):
         # both inputs are OrderedDicts from parity to vec
         res = self._lr_solver(c_tm1.values(), c_t.values())
-        f0, f1 = res[:2]
+        f = list(res[:2])
 
-        print 'backwards'
-        print f0, f1
+        print '\nbackward'
+        print f
 
         # now need to untangle iz terms. can do this because we know i>=0.
-        # This allows taking logs and solving simple 4x4 linear system
+        # This allows taking logs and solving 4x4 linear system
 
+        # iz = [i0z0, i0(z1^1.1), i1(z0^1.1), i1z1]
         iz = np.array(res[2:])
         sign = np.sign(iz)
+        assert np.all(sign[:2] == sign[2:])
+
         liz = np.log(np.abs(iz))
-        iz_res = self._iz_solver(*liz)
-        print iz_res
+        iz_res = np.exp(self._iz_solver(*liz))
+        i, z = list(iz_res[:2]), list(iz_res[2:])
+        z[0] *= sign[0]
+        z[1] *= sign[1]
+        # now have f, i, z recovered!
+
+        # undo activations
+        pre_f = isigmoid(np.concatenate(f))
+        pre_i = isigmoid(np.concatenate(i))
+        pre_z = itanh(np.concatenate(z))
+
+        out = np.concatenate([pre_f, pre_i, pre_z])
+        inp = np.linalg.lstsq(self.W[:3 * self.N], out)[0]
+
+        o = sigmoid(np.dot(self.W[-self.N:], inp))
+        c = self.combine_c(c_t)
+        h = o * c
+        print h
+        print self.h
 
 def build_solver(parity):
     N = len(parity)
@@ -133,6 +167,7 @@ def build_solver(parity):
     return solver
 
 if __name__ == '__main__':
+    np.random.seed(2017)
     N = 4
     m = Lstm(N)
     c_tm1 = copy.copy(m.c)
